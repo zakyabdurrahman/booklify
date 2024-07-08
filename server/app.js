fa5cf433c67df65d82d7c6471452f07b62857f6b
zakyabdurrahman <meansproducer02@gmail.com>
2024-07-09 06:02:36 +0700

    added app.js

---
+++ server/app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+
+app.use(express.json());
+
+
+
+app.listen(PORT, () => {
+    console.log(`Online on http://localhost:${PORT}`);
+})

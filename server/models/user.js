d1b8b6b2799ba4b52365bfdf2d9cb974e83b9ea1
zakyabdurrahman <zakyabdurrahman10@gmail.com>
2024-07-08 17:46:13 +0700

    user migration

---
+++ server/models/user.js
@@ -0,0 +1,61 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  User.init({
+    username: {
+      type: DataTypes.STRING,
+      unique: {
+        msg: "Username must be unique"
+      },
+      allowNull: false,
+      validate: {
+        notEmpty: {
+          msg: "Username is required"
+        },
+        notNull: {
+          msg: "Username is required"
+        }
+      }
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: {
+          msg: "Password is required"
+        },
+        notNull: {
+          msg: "Password is required"
+        }
+      }
+    },
+    profileUrl: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: {
+          msg: "Profile image is required"
+        },
+        notNull: {
+          msg: "Profile image is required"
+        }
+      }
+    }
+  }, {
+    sequelize,
+    modelName: 'User',
+  });
+  return User;
+};

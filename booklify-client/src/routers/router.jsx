653612db7c0dc8ab18e9fc0a683c7f0b59b1326d
zakyabdurrahman <meansproducer02@gmail.com>
2024-07-09 21:29:19 +0700

    finished register page

--- booklify-client/src/routers/router.jsx
+++ booklify-client/src/routers/router.jsx
@@ -1,15 +1,22 @@
 import { createBrowserRouter } from "react-router-dom";
 import HomePage from "../views/HomePage";
 import LoginPage from "../views/LoginPage";
+import RegisterPage from "../views/RegisterPage";
+import authentication from "../../middlewares/authentication";
 
 const router = createBrowserRouter([
     {
         path: "/",
-        element: <HomePage/>
+        element: <HomePage/>,
+        loader: authentication
     },
     {
         path: "/login",
         element: <LoginPage/>
+    },
+    {
+        path: "/register",
+        element: <RegisterPage/>
     }
 ])
 
--- booklify-client/src/views/RegisterPage.jsx
+++ booklify-client/src/views/RegisterPage.jsx
@@ -1,52 +1,27 @@
 import axios from "axios";
-import { Link, useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom"
 import { toast } from "react-toastify";
 import baseUrl from "../utils/constant";
-import { GoogleLogin } from "@react-oauth/google";
 
-export default function LoginPage() {
+function RegisterPage() {
 
     const navigate = useNavigate();
 
-    async function handleLogin(e) {
+    async function handleRegister(e) {
         e.preventDefault();
-
-        const username = e.target[0].value;
-        const password = e.target[1].value;
-
         try {
-            const response = await axios.post(`${baseUrl}/login`, {
-                username,
-                password
+            const {data} = await axios.post(`${baseUrl}/register`, {
+                username: e.target[0].value,
+                profileUrl: e.target[1].value,
+                password: e.target[2].value
             })
-            
-            const {access_token} = response.data;
-            localStorage.setItem('access_token', access_token);
+
+            localStorage.setItem('access_token', data.access_token);
             navigate('/');
-
-
+            
         } catch (error) {
             console.log(error);
             toast.error(error.response.data.message);
-        }
-    }
-
-    async function googleLogin(googleResponse) {
-        try {
-            const {data} = await axios.post(`${baseUrl}/google-login`, null, {
-                headers: {
-                    token: googleResponse.credential
-                }
-            })
-
-            const {access_token} = data;
-            localStorage.setItem('access_token', access_token);
-            
-            console.log(data);
-            navigate('/');
-
-        } catch (error) {
-            toast.error(error.response.data.message)
         }
     }
 
@@ -55,9 +30,9 @@ export default function LoginPage() {
             <div className="h-screen flex w-screen justify-center items-center">
                 <div className="card bg-base-100 w-96 shadow-xl">
                     <div className="card-body">
-                        <h2 className="text-center font-bold text-xl">Login to Booklify</h2>
-                        
-                        <form onSubmit={handleLogin}>
+                        <h2 className="text-center font-bold text-xl">Welcome to Booklify</h2>
+                        <p className="text-center">Register your account here</p>
+                        <form onSubmit={handleRegister}>
                             <label className="input input-bordered flex items-center gap-2 mt-6">
                                 <svg
                                     xmlns="http://www.w3.org/2000/svg"
@@ -68,6 +43,10 @@ export default function LoginPage() {
                                     d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z" />
                                 </svg>
                                 <input type="text" className="grow" placeholder="Username" />
+                            </label>
+                             <label className="input input-bordered flex items-center gap-2 mt-6">
+                               
+                                <input type="text" className="grow" placeholder="Profile Image URL" />
                             </label>
                             <label className="input input-bordered flex items-center gap-2 mt-6">
                                 <svg
@@ -83,18 +62,17 @@ export default function LoginPage() {
                                 <input type="password" className="grow" placeholder="Password" />
                             </label>
                             <div className="card-actions justify-center mt-4">
-                                <button className="btn btn-primary">Login</button>
+                                <button className="btn btn-primary">Signup</button>
                             </div>
                         </form>
-                        <div className="divider">OR</div>
-                        <div className="flex justify-center">
-                            <GoogleLogin onSuccess={googleLogin} onError={() => toast.error('Google login failed. Please contact Sundar Pichai for help')}/>
-                        </div>
                         
-                        <p className="text-center text-sm mt-2">Don't have an account? <Link className="text-blue-600" to="/register">Signup</Link></p>
+                        
+                        <p className="text-center text-sm mt-2">Already have an account? <Link to="/login" className="text-blue-600">Login</Link> </p>
                     </div>
                 </div>
             </div>
         </>
     )
 }
+
+export default RegisterPage
